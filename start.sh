5f58f7d9e99083f9435e1757935c8756c524c433
Lorenzo <lorenzo.carbonell.cerezo@gmail.com>
2021-09-24 06:11:40 +0200

    add debug environment variable

--- start.sh
+++ start.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
 sudo /bin/chown -R poduser:poduser /crontab
-crond -c /crontab -f -d 1
+
+# Usage: crond -fbS -l N -d N -L LOGFILE -c DIR
+# 
+#     -f  Foreground
+#     -b  Background (default)
+#     -S  Log to syslog (default)
+#     -l N    Set log level. Most verbose:0, default:8
+#     -d N    Set log level, log to stderr
+#     -L FILE Log to FILE
+#     -c DIR  Cron dir. Default:/var/spool/cron/crontabs
+
+DEBUG=${8:-$DEBUG}
+crond -c /crontab -f -d ${DEBUG}
 
